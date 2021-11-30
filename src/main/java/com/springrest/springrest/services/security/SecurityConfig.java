c8e80c1b5db3e0a91367e49a1b12b775db1b3d84
obayed <obayed@eatlbd.com>
2021-11-30 18:16:02 +0600

    CRUD with redux and saga worked version from office

--- src/main/java/com/springrest/springrest/services/security/SecurityConfig.java
+++ src/main/java/com/springrest/springrest/services/security/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors();
+        http.cors().disable();
     /*and()
                 .authorizeRequests()
                 .antMatchers(SWAGGER).permitAll()
